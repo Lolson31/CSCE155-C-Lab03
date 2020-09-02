2fe1f2c0aa7ac740c0295b99f4e8ca0962d44279
Lolson31 <69780105+Lolson31@users.noreply.github.com>
2020-09-01 23:02:20 -0500

    Update leapYear.c

--- leapYear.c
+++ leapYear.c
@@ -115,28 +115,10 @@ int isLeapYear(int year)
 {
   if (year % 4 == 0 && year %100 != 0 || year % 400 == 0)
   {
-    return 0;
-  }
-  else
-  {
     return 1;
   }
-  /**
-  if (year % 400 == 0)
-  {
-    0;
-  }
-  else if (year % 100 != 0)
-  {
-    0;
-  }
-  else if (year % 4)
-  {
-    0;
-  }
   else
   {
-    1;
+    return 0;
   }
-  */
 }
