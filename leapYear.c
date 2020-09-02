632523a8ba448a9b48af29c0d2513749b10af89c
Lolson31 <69780105+Lolson31@users.noreply.github.com>
2020-09-01 22:33:52 -0500

    Update leapYear.c

--- leapYear.c
+++ leapYear.c
@@ -113,7 +113,7 @@ int main(int argc, char **argv)
 
 int isLeapYear(int year)
 {
-    if (((year % 4) == 0) && ((year % 100 == !0) || (year % 400 == 0)))
+    if (((year % 4) == 0) && ((year % 400 == 0) || (year % 100 != 0)))
     {
       1;
     }
